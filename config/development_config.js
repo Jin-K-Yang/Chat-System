4411eba269ad0a39250ecafe129c52ad1dd22045
Jin Kai Yang <ken8989580@gmail.com>
2020-04-11 23:41:35 +0800

    config

--- config/development_config.js
+++ config/development_config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
 	connect_info : {
 		host : process.env.HOST,
-		user : process.env.USER,
+		user : process.env.NAME,
 		password : process.env.PASSWORD,
 		database : process.env.DATABASE
 	}

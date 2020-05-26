60754892a1f7870b4a6cbf46d7a4ec4340714f73
bevan222 <bevan2628@gmail.com>
2020-05-26 10:55:03 +0800

    new page

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
+
+
+
+
 //get any variable needed
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var memberRouter = require("./routes/member");
 var verifyRouter = require("./routes/verification");
+
+//new
+//var roomListRouter = require("./routes/roomList");
 
 var app = express();
 
@@ -33,6 +40,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/member", memberRouter);
 app.use("/verify", verifyRouter);
+//app.use("/roomList",roomListRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -51,3 +59,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
 const UsersModifyMethod = require("../controllers/users_controller");
 const varify = require("../controllers/verification_controller.js");
 
@@ -14,6 +17,36 @@ router.get("/personalSet", usersMethod.getpersonalSetPage);
 
 router.get("/:name/profile", usersMethod.getProfile);
 
+router.get('/roomList', function(req, res, next) {
+  res.render('chatRoomList', { 
+  	title: 'ETestejs',
+  	name: 'bevan222'
+
+  	});
+});
+
+
+router.get('/chatRoom', function(req, res, next) {
+  res.render('room', { 
+  	title: 'ETestejs',
+  	name: 'bevan222'
+
+  	});
+});
+
 router.put("/update", usersMethod.putUpdate);
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
